035b9cf8cc4a086bad4b5c9611b109ca1f31cd90
James Ferrell <hello@jamesferrell.me>
2019-06-09 13:57:47 -0400

    adds svg-loader

--- gridsome.config.js
+++ gridsome.config.js
@@ -99,5 +99,14 @@ module.exports = {
       externalLinksRel: ["nofollow"],
       anchorClassName: "heading-anchor-link"
     }
+  },
+
+  // svg loader
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
   }
 }
