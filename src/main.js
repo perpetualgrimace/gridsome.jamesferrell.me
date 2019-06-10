b54c8e792c6544816997e298468905321c63d9c8
James Ferrell <hello@jamesferrell.me>
2019-06-09 20:00:03 -0400

    cleans up main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
+import "@/scss/main.scss";
 
 export default function (Vue, { router, head, isClient }) {
-  // global meta
-  // head.link.push({
-  //   rel: "stylesheet",
-  //   href: ""
-  // })
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
 
@@ -22,5 +18,3 @@ export default function (Vue, { router, head, isClient }) {
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 }
-
-import "@/scss/main.scss";
