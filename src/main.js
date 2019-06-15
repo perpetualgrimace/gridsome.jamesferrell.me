02fe098a7adc38e24c82b1feba1e603d9de818ec
James Ferrell <hello@jamesferrell.me>
2019-06-15 17:05:58 -0400

    fixes typekit import

--- src/main.js
+++ src/main.js
@@ -6,17 +6,10 @@ import "@/scss/main.scss";
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
+  Vue.component("Layout", DefaultLayout)
 
-  // typekit
-  // if (typeof document !== "undefined") {
-  //   (function(d) {
-  //     var config = {
-  //       kitId: 'bar1poh',
-  //       scriptTimeout: 3000,
-  //       async: true
-  //     },
-  //     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  //   })(document);
-  // };
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://use.typekit.net/bar1poh.css"
+  })
 }
