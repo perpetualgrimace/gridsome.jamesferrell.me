3ec8586003f7c75eeb7092ef2a5a7a8bcdfbad82
perpetualgrimace <hello@jamesferrell.me>
2020-08-10 13:10:59 -0400

    fixes mobile nav

--- src/components/HamBurger.vue
+++ src/components/HamBurger.vue
@@ -1,138 +1,143 @@
 <script>
-  export default {
-    name: "HamBurger",
-    props: {
-      onClick: { type: Function },
-      menuOpen: { default: false }
-    }
-  }
+	export default {
+		name: "HamBurger",
+		props: {
+			onClick: { type: Function },
+			menuOpen: { default: false }
+		}
+	}
 </script>
 
 
 <template>
-  <a
-    class="hamburger u-hide-above-s"
-    :class="menuOpen ? 'is-active' : 'is-inactive'"
-    @click="onClick($event)"
-    data-nav="toggle"
-    href="#footer-nav"
-  >
-    <span class="hamburger-text">menu</span>
-    <div class="hamburger-icon" :class="menuOpen ? 'is-active' : 'is-inactive'">
-      <span class="hamburger-icon-bun hamburger-icon-bun-top" />
-      <span class="hamburger-icon-bun hamburger-icon-bun-patty" />
-      <span class="hamburger-icon-bun hamburger-icon-bun-bottom" />
-    </div>
-  </a>
+	<a
+		class="hamburger u-hide-above-s"
+		:class="menuOpen ? 'is-active' : 'is-inactive'"
+		@click="onClick($event)"
+		data-nav="toggle"
+		href="#footer-nav"
+	>
+		<span class="hamburger-text">menu</span>
+		<div class="hamburger-icon" :class="menuOpen ? 'is-active' : 'is-inactive'">
+			<span class="hamburger-icon-bun hamburger-icon-bun-top" />
+			<span class="hamburger-icon-bun hamburger-icon-bun-patty" />
+			<span class="hamburger-icon-bun hamburger-icon-bun-bottom" />
+		</div>
+	</a>
 </template>
 
 
 <style lang="scss">
-  // nav toggle button container
-  .hamburger {
-    @include box-shadow(rgba($black, 0.25));
-    // positioning
-    position: absolute;
-    top: 0.675rem;
-    right: $gutter;
-    padding: 0 10px 0 9px; // somewhat fudged
-    line-height: 30px;
-    height: 30px;
-    max-width: 10rem;
-    z-index: 1; // position above logo on tiny viewports
+	// nav toggle button container
+	.hamburger {
+		@include box-shadow(rgba($black, 0.25));
+		// positioning
+		position: absolute;
+		top: 0.675rem;
+		right: $gutter;
+		padding: 0 10px 0 9px; // somewhat fudged
+		line-height: 30px;
+		height: 30px;
+		max-width: 10rem;
+		z-index: 1; // position above logo on tiny viewports
 
-    // menu toggle text
-    .hamburger-text {
-      font-size: 18px; // set in pixels like the icon and links
-      color: inherit;
-    }
+		// menu toggle text
+		.hamburger-text {
+			font-size: 18px; // set in pixels like the icon and links
+			color: inherit;
+		}
 
-    // click me
-    &.is-inactive {
-      transition-property: color; // override default link style
+		// click me
+		&.is-inactive {
+			transition-property: color; // override default link style
 
-      &:hover, &:focus {
-        .hamburger-icon {
-          transform: scale(1.125);
-        }
-      }
-    }
+			&:hover, &:focus {
+				.hamburger-icon {
+					transform: scale(1.125);
+				}
+			}
+		}
 
-    // nav-toggle active state
-    &.is-active {
-      // sized to menu item height
-      width: calc(100vh / 7);
-      height: calc(100vh / 7);
-      padding: 0;
-      // absolutely position, then offset in a way that's transitionable
-      position: absolute;
-      top: -$gutter;
-      right: -$gutter;
-      transform: translate(-$gutter, $gutter);
-      // centered X
-      display: flex;
-      background-color: transparent;
-      border-radius: $radius-lg $radius-lg 0 0;
-      box-shadow: none;
-      z-index: 31;
+		// nav-toggle active state
+		&.is-active {
+			// sized to menu item height
+			width: calc(100vh / 7);
+			height: calc(100vh / 7);
+			padding: 0;
+			// absolutely position, then offset in a way that's transitionable
+			position: absolute;
+			top: -$gutter;
+			right: -$gutter;
+			transform: translate(-$gutter, $gutter);
+			// centered X
+			display: flex;
+			background-color: transparent;
+			border-radius: $radius-lg $radius-lg 0 0;
+			box-shadow: none;
+			z-index: 31;
 
-      &:focus {
-        outline-offset: 2px;
-      }
+			@media (max-height: $xxs - 0.001) {
+				right: 0;
+				top: 0;
+			}
 
-      .hamburger-text {
-        @include visually-hidden;
-        color: transparent;
-      }
+			&:focus {
+				outline-offset: 2px;
+			}
 
-      .hamburger-icon {
-        margin: auto;
-        width: 1rem;
-        height: 1rem;
-        transform: scale(1.5) rotate(-90deg) translateX(0.375em); // frickin' fudged
-      }
-    }
+			.hamburger-text {
+				@include visually-hidden;
+				color: transparent;
+			}
 
-    &:hover *, 
-    &:focus * {
-      color: $white;
-    }
-  }
+			.hamburger-icon {
+				margin: auto;
+				width: 1rem;
+				height: 1rem;
+				transform: scale(1.5) rotate(-90deg) translateX(0.375em); // frickin' fudged
+			}
+		}
 
-  // hamburger icon container
-  .hamburger-icon {
-    display: inline-block;
-    margin-left: 0.4rem;
-    transition: transform $timing;
-  }
+		&:hover *, 
+		&:focus * {
+			color: $white;
+		}
+	}
 
-  // hamburger initial state
-  .hamburger-icon-bun {
-    display: block;
-    width: 16px;
-    height: 2px;
-    border-radius: 1px;
-    background-color: currentColor;
-    transition: transform $timing;
+	// hamburger icon container
+	.hamburger-icon {
+		display: inline-block;
+		margin-left: 0.4rem;
+		transition: transform $timing;
+	}
 
-    // stacked buns
-    & + & {
-      margin-top: 3px;
-    }
-  }
+	// hamburger initial state
+	.hamburger-icon-bun {
+		display: block;
+		width: 16px;
+		height: 2px;
+		border-radius: 1px;
+		background-color: currentColor;
+		transition: transform $timing;
 
-  // hamburger active state
-  .hamburger-icon.is-active {
-    transform: translateX(5px);
+		// stacked buns
+		& + & {
+			margin-top: 3px;
+		}
+	}
 
-    .hamburger-icon-bun-top {
-      transform: rotateZ(45deg) translateY(7px);
-    }
-    .hamburger-icon-bun-patty {
-      opacity: 0;
-    }
-    .hamburger-icon-bun-bottom {
-      transform: rotateZ(-45deg) translateY(-7px);
-    }
-  }
+	// hamburger active state
+	.hamburger-icon.is-active {
+		transform: translateX(5px);
+
+		.hamburger-icon-bun-top {
+			transform: rotateZ(45deg) translateY(7px);
+		}
+		.hamburger-icon-bun-patty {
+			opacity: 0;
+		}
+		.hamburger-icon-bun-bottom {
+			transform: rotateZ(-45deg) translateY(-7px);
+		}
+	}
 </style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,254 +1,262 @@
 <script>
-  import site from "../../content/site.json";
-  import HamBurger from "./HamBurger";
-  import MainLogo from "./MainLogo";
+	import site from "../../content/site.json";
+	import HamBurger from "./HamBurger";
+	import MainLogo from "./MainLogo";
 
-  export default {
-    name: "MainNav",
-    components: {
-      HamBurger,
-      MainLogo
-    },
-    data() {
-      return {
-        site,
-        menuOpen: false
-      }
-    },
-    methods: {
-      toggleMenu: function(e) {
-        if (e) e.preventDefault();
-        this.menuOpen = !this.menuOpen;
-      }
-    }
-  };
+	export default {
+		name: "MainNav",
+		components: {
+			HamBurger,
+			MainLogo
+		},
+		data() {
+			return {
+				site,
+				menuOpen: false
+			}
+		},
+		methods: {
+			toggleMenu: function(e) {
+				if (e) e.preventDefault();
+				this.menuOpen = !this.menuOpen;
+			}
+		}
+	};
 </script>
 
 
 <template>
-  <div class="main-nav-container">
-    <nav class="main-nav" role="navigation">
-      <!-- skip link -->
-      <a class="button-inverted skip-link u-visually-hidden" href="#main">Skip to content</a>
+	<div class="main-nav-container">
+		<nav class="main-nav" role="navigation">
+			<!-- skip link -->
+			<a class="button-inverted skip-link u-visually-hidden" href="#main">Skip to content</a>
 
-      <MainLogo :tabbable="!menuOpen" />
-      
-      <!-- nav menu toggle for small screens -->
-      <HamBurger :onClick="toggleMenu" :menuOpen="menuOpen" />
+			<MainLogo :tabbable="!menuOpen" />
+			
+			<!-- nav menu toggle for small screens -->
+			<HamBurger :onClick="toggleMenu" :menuOpen="menuOpen" />
 
 
-      <!-- main nav -->
-      <ul id="nav" class="main-nav-list" :class="menuOpen ? 'is-expanded' : 'is-collapsed'">
-        <li class="main-nav-item u-hide-above-s">
-          <g-link to="/" class="main-nav-link" exact-active-class="is-active">
-            home
-          </g-link>
-        </li>
-        <li class="main-nav-item" v-for="nav in site.mainNav">
-          <g-link :to="nav.link" class="main-nav-link" active-class="is-active" exact-active-class="is-active">
-            {{ nav.title }}
-          </g-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+			<!-- main nav -->
+			<ul id="nav" class="main-nav-list" :class="menuOpen ? 'is-expanded' : 'is-collapsed'">
+				<li class="main-nav-item u-hide-above-s">
+					<g-link to="/" class="main-nav-link" exact-active-class="is-active">
+						home
+					</g-link>
+				</li>
+				<li class="main-nav-item" v-for="nav in site.mainNav">
+					<g-link :to="nav.link" class="main-nav-link" active-class="is-active" exact-active-class="is-active">
+						{{ nav.title }}
+					</g-link>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </template>
 
 
 <style lang="scss">
 
-  // default positioning and theming
-  .main-nav-container {
-    top: 0;
-    z-index: 11;
-    margin-bottom: 0;
-    background-color: $brand-black;
+	// default positioning and theming
+	.main-nav-container {
+		top: 0;
+		z-index: 11;
+		margin-bottom: 0;
+		background-color: $brand-black;
 
-    // background color
-    @media (min-width: $s) {
-      background-color: $white;
-    }
+		// background color
+		@media (min-width: $s) {
+			background-color: $white;
+		}
 
-    // fixed nav on bigger screens
-    @include bp-fixed-nav {
-      @include box-shadow($shadow);
-      position: fixed;
+		// fixed nav on bigger screens
+		@include bp-fixed-nav {
+			@include box-shadow($shadow);
+			position: fixed;
 
-      & + .main {
-        margin-top: $nav-height;
-      }
-    }
-  }
-
-
-  ////////////////////////////////////////////
-  // main nav element
-  ////////////////////////////////////////////
-
-  // positioning
-  .main-nav {
-    height: $nav-height;
-    display: flex;
-
-    // layout adjustments on bigger screens
-    @media (min-width: $m) {
-      // max-width: $max-main-width;
-    }
-
-    // offset nav-link padding on 3 column layout
-    @media (min-width: $l) {
-      padding-left: 0.5rem;
-    }
-  }
-
-  // base style for nav elements
-  .main-nav * {
-    @include body-semibold-font;
-    @include font-smoothing;
-    font-size: $epsilon;
-    text-transform: uppercase;
-    text-decoration: none;
-    color: $gray;
-  }
+			& + .main {
+				margin-top: $nav-height;
+			}
+		}
+	}
 
 
-  ////////////////////////////////////////////
-  // main nav list
-  ////////////////////////////////////////////
+	////////////////////////////////////////////
+	// main nav element
+	////////////////////////////////////////////
 
-  .main-nav-list {
-    // small screens
-    @media (max-width: $s - 0.001) {
-      @include dropdown-hidden;
-      @include box-shadow-lg;
-      // max-width: $sidebar-width;
-      display: flex;
-      flex-direction: column;
-      max-height: $nav-height;
-      z-index: -1;
+	// positioning
+	.main-nav {
+		height: $nav-height;
+		display: flex;
 
-      &.is-collapsed .main-nav-link {
-        display: none;
-      }
+		// layout adjustments on bigger screens
+		@media (min-width: $m) {
+			// max-width: $max-main-width;
+		}
 
-      // expanded menu state
-      &.is-expanded {
-        @include dropdown-visible;
-        @include absolute-expand;
-        background-color: rgba($brand-black, 0.95);
-        backdrop-filter: blur(4px);
-        position: fixed;
-        max-height: 100%;
-        z-index: 30;
-        transition:
-          opacity $timing,
-          transform $timing;
-      }
-    }
+		// offset nav-link padding on 3 column layout
+		@media (min-width: $l) {
+			padding-left: 0.5rem;
+		}
+	}
 
-    // centered between logo and right screen edge
-    @media (min-width: $s) {
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    // left-aligned
-    @media (min-width: $m) {
-      margin-left: $gutter;
-    }
-  }
+	// base style for nav elements
+	.main-nav * {
+		@include body-semibold-font;
+		@include font-smoothing;
+		font-size: $epsilon;
+		text-transform: uppercase;
+		text-decoration: none;
+		color: $gray;
+	}
 
 
-  ////////////////////////////////////////////
-  // main nav items
-  ////////////////////////////////////////////
+	////////////////////////////////////////////
+	// main nav list
+	////////////////////////////////////////////
 
-  // nav items
-  .main-nav-item {
-    width: 100%;
-    height: auto;
-    float: left;
+	.main-nav-list {
+		// small screens
+		@media (max-width: $s - 0.001) {
+			@include dropdown-hidden;
+			@include box-shadow-lg;
+			// max-width: $sidebar-width;
+			display: flex;
+			flex-direction: column;
+			max-height: $nav-height;
+			z-index: -1;
 
-    @media (max-width: $s - 0.001) {
-      flex: 1 1 auto;
-      display: flex;
+			&.is-collapsed .main-nav-link {
+				display: none;
+			}
 
-      &:last-of-type {
-        border-radius: 0 0 $radius-lg $radius-lg;
-        overflow: hidden;
-      }
-    }
+			// expanded menu state
+			&.is-expanded {
+				@include dropdown-visible;
+				@include absolute-expand;
+				background-color: rgba($brand-black, 0.95);
+				backdrop-filter: blur(4px);
+				position: fixed;
+				max-height: 100%;
+				z-index: 30;
+				transition:
+					opacity $timing,
+					transform $timing;
+			}
+		}
 
-    // large screen style
-    @media (min-width: $s) {
-      width: auto;
-      display: block;
-      background-color: $white;
-    }
-  }
+		// centered between logo and right screen edge
+		@media (min-width: $s) {
+			margin-left: auto;
+			margin-right: auto;
+		}
 
+		// left-aligned
+		@media (min-width: $m) {
+			margin-left: $gutter;
+		}
 
-  ////////////////////////////////////////////
-  // main nav links
-  ////////////////////////////////////////////
-
-  // nav links
-  .main-nav-link {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    flex: 1 1 100%;
-    width: 100%;
-    z-index: 1;
-    overflow: hidden;
-
-    @media (max-width: $s - 0.001) {
-      font-size: calc(2vh + #{$milli});
-      padding: 0.25em $gutter * 2;
-
-      &:focus {
-        outline-offset: -2px;
-      }
-    }
-
-    // extra padding where screen real estate allows
-    @media (min-width: $s) {
-      padding: 0 rem(9);
-      line-height: $nav-height; // vertically center
-      font-size: $epsilon;
-
-      // interactions
-      &:not(.is-active) {
-        @include fancy-nav-hover;
-      }
-    }
-
-    // active page
-    &.is-active {
-      color: $dark-2;
-    }
-  }
+		@media (max-height: $xxs - 0.001) {
+			flex-direction: row;
+			flex-wrap: wrap;
+		}
+	}
 
 
-  ////////////////////////////////////////////
-  // skip link
-  ////////////////////////////////////////////
+	////////////////////////////////////////////
+	// main nav items
+	////////////////////////////////////////////
 
-  .skip-link {
-    top: $nav-height + 0.13rem; // position under nav bar
-    z-index: 8;
-    transform: translateX(-100%);
+	// nav items
+	.main-nav-item {
+		width: 100%;
+		height: auto;
+		float: left;
 
-    @media (min-width: $l) {
-      left: 1.5rem;
-    }
+		@media (max-width: $s - 0.001) {
+			flex: 1 1 auto;
+			display: flex;
 
-    // focus
-    &:focus {
-      clip: auto;
-      height: auto;
-      width: auto;
-      transform: none;
-    }
-  }
+			&:last-of-type {
+				border-radius: 0 0 $radius-lg $radius-lg;
+			}
+		}
+
+		// large screen style
+		@media (min-width: $s) {
+			width: auto;
+			display: block;
+			background-color: $white;
+		}
+
+		@media (max-height: $xxs - 0.001) {
+			flex-basis: 50%;
+		}
+	}
+
+
+	////////////////////////////////////////////
+	// main nav links
+	////////////////////////////////////////////
+
+	// nav links
+	.main-nav-link {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		flex: 1 1 100%;
+		width: 100%;
+		z-index: 1;
+		overflow: hidden;
+
+		@media (max-width: $s - 0.001) {
+			font-size: calc(2vh + #{$milli});
+			padding: 0.25em $gutter * 2;
+
+			&:focus {
+				outline-offset: -2px;
+			}
+		}
+
+		// extra padding where screen real estate allows
+		@media (min-width: $s) {
+			padding: 0 rem(9);
+			line-height: $nav-height; // vertically center
+			font-size: $epsilon;
+
+			// interactions
+			&:not(.is-active) {
+				@include fancy-nav-hover;
+			}
+		}
+
+		// active page
+		&.is-active {
+			color: $dark-2;
+		}
+	}
+
+
+	////////////////////////////////////////////
+	// skip link
+	////////////////////////////////////////////
+
+	.skip-link {
+		top: $nav-height + 0.13rem; // position under nav bar
+		z-index: 8;
+		transform: translateX(-100%);
+
+		@media (min-width: $l) {
+			left: 1.5rem;
+		}
+
+		// focus
+		&:focus {
+			clip: auto;
+			height: auto;
+			width: auto;
+			transform: none;
+		}
+	}
 </style>
