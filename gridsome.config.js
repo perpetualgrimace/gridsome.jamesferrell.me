13a8d557ef75cdb96db8327c486adda90747619a
perpetualgrimace <hello@jamesferrell.me>
2023-09-13 17:07:15 -0400

    fixes dependency hell for builds too?

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,24 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run "gridsome develop"
 
+const crypto = require("crypto");
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash("md4");
+} catch (e) {
+  console.warn(
+    'Crypto "MD4" is not supported anymore by this Node.js version'
+  );
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === "md4" ? "md5" : alg, opts);
+  };
+}
+
 var site = require("./content/site.json");
 
 module.exports = {
@@ -23,26 +41,41 @@ module.exports = {
           @import "@/scss/1-utils/_variables.scss";
           @import "@/scss/1-utils/_mixins.scss";
           @import "@/scss/1-utils/_keyframes.scss";
-        `
-      }
-    }
+        `,
+      },
+    },
   },
 
   // default image settings
   images: {
-    defaultQuality: 90 // doesn't appear to do anything?
+    defaultQuality: 90, // doesn't appear to do anything?
   },
 
   // icons
   icon: {
     favicon: {
       src: "./src/favicon.png",
-      sizes: [16, 32, 64]
+      sizes: [16, 32, 64],
     },
     touchicon: {
-      sizes: [57, 114, 72, 144, 60, 120, 76, 152, 180, 192, 160, 96, 16, 32],
-      src: "./src/favicon.png"
-    }
+      sizes: [
+        57,
+        114,
+        72,
+        144,
+        60,
+        120,
+        76,
+        152,
+        180,
+        192,
+        160,
+        96,
+        16,
+        32,
+      ],
+      src: "./src/favicon.png",
+    },
   },
 
   plugins: [
@@ -51,15 +84,15 @@ module.exports = {
       options: {
         paths: ["/"],
         width: 1440,
-        height: 900
-      }
+        height: 900,
+      },
     },
     {
       use: "@gridsome/plugin-sitemap",
       options: {
         cacheTime: 600000, // default
         // exclude: ["/exclude-me"],
-      }
+      },
     },
 
     /////////////////////////////////////////
@@ -72,8 +105,8 @@ module.exports = {
       options: {
         path: "content/**/home.md",
         typeName: "Index",
-        route: "/"
-      }
+        route: "/",
+      },
     },
 
     // project list
@@ -82,8 +115,8 @@ module.exports = {
       options: {
         path: "content/**/project-list.md",
         typeName: "Projects",
-        route: "/projects"
-      }
+        route: "/projects",
+      },
     },
     // projects
     {
@@ -91,8 +124,8 @@ module.exports = {
       options: {
         path: "content/**/project-detail.md",
         typeName: "Project",
-        route: "/projects/:slug"
-      }
+        route: "/projects/:slug",
+      },
     },
 
     // about
@@ -102,8 +135,8 @@ module.exports = {
         baseDir: "content/3-about",
         typeName: "About",
         template: "./src/templates/About.vue",
-        route: "/about"
-      }
+        route: "/about",
+      },
     },
 
     // resume
@@ -113,8 +146,8 @@ module.exports = {
         baseDir: "content/4-resume",
         typeName: "Resume",
         template: "./src/templates/Resume.vue",
-        route: "/resume"
-      }
+        route: "/resume",
+      },
     },
 
     // blog
@@ -123,8 +156,8 @@ module.exports = {
       options: {
         path: "content/5-blog/blog.md",
         typeName: "Blog",
-        route: "/blog"
-      }
+        route: "/blog",
+      },
     },
 
     // blog posts
@@ -134,8 +167,8 @@ module.exports = {
         baseDir: "content/5-blog/articles",
         typeName: "Article",
         template: "./src/templates/Article.vue",
-        route: "/blog/:slug"
-      }
+        route: "/blog/:slug",
+      },
     },
 
     // contact
@@ -145,8 +178,8 @@ module.exports = {
         baseDir: "content/6-contact",
         typeName: "Contact",
         template: "./src/templates/Contact.vue",
-        route: "/contact"
-      }
+        route: "/contact",
+      },
     },
 
     // uses
@@ -156,8 +189,8 @@ module.exports = {
         baseDir: "content/uses",
         typeName: "Uses",
         template: "./src/templates/Uses.vue",
-        route: "/uses"
-      }
+        route: "/uses",
+      },
     },
 
     // blocks
@@ -165,46 +198,44 @@ module.exports = {
       use: "@gridsome/source-filesystem",
       options: {
         path: "content/blocks/colophon.md",
-        typeName: "Colophon"
-      }
+        typeName: "Colophon",
+      },
     },
     {
       use: "@gridsome/source-filesystem",
       options: {
         path: "content/blocks/footer.md",
-        typeName: "Footer"
-      }
+        typeName: "Footer",
+      },
     },
     {
       use: "@gridsome/source-filesystem",
       options: {
         path: "content/blocks/bullet-lists/*.md",
-        typeName: "BulletList"
-      }
+        typeName: "BulletList",
+      },
     },
     {
       use: "@gridsome/source-filesystem",
       options: {
         path: "content/blocks/definition-lists/*.md",
-        typeName: "DefinitionList"
-      }
-    }
+        typeName: "DefinitionList",
+      },
+    },
   ],
 
   // markdown parsing
   transformers: {
     remark: {
       externalLinksTarget: false,
-      externalLinksRel: ["nofollow"]
-    }
+      externalLinksRel: ["nofollow"],
+    },
   },
 
   // svg loader
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
-  }
-}
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
+};
