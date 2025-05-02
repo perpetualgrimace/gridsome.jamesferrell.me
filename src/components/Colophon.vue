8b74cdd68951930ee07aa992dc2c66d575ec53c7
James Ferrell <hello@jamesferrell.me>
2025-05-02 19:31:30 -0400

    updates main photo

--- src/components/Colophon.vue
+++ src/components/Colophon.vue
@@ -7,193 +7,192 @@
 </static-query>
 
 <script>
-	import site from "../../content/site.json";
+import site from "../../content/site.json";
 
-	export default {
-		name: "Colophon",
-		props: {
-			headline: { default: site.title },
-			classes:  { default: null },
-			img:      { default: "/images/jamesatar.jpg" },
-			img2x:    { default: "/images/jamesatar@2x.jpg" }
-		},
-		data() {
-			return {
-				site,
-				faceClicked: false
-			}
-		},
-		methods: {
-			onFaceClick: function() {
-				if (!this.faceClicked) alert("Hey, that's not very nice 😬");
-				this.faceClicked++;
-				if (this.faceClicked === 10) alert("Stahp! 😫");
-				if (this.faceClicked >= 20) alert("kbye 👋");
-			}
-		}
-	};
+export default {
+  name: "Colophon",
+  props: {
+    headline: { default: site.title },
+    classes: { default: null },
+    img: { default: "/images/jamesatar.png" },
+    img2x: { default: "/images/jamesatar@2x.png" },
+  },
+  data() {
+    return {
+      site,
+      faceClicked: false,
+    };
+  },
+  methods: {
+    onFaceClick: function() {
+      if (!this.faceClicked) alert("Hey, that's not very nice 😬");
+      this.faceClicked++;
+      if (this.faceClicked === 10) alert("Stahp! 😫");
+      if (this.faceClicked >= 20) alert("kbye 👋");
+    },
+  },
+};
 </script>
 
-
 <template>
-	<div class="colophon brand-theme" :class="classes">
-		<div class="colophon-inner">
+  <div class="colophon brand-theme" :class="classes">
+    <div class="colophon-inner">
+      <!-- my face -->
+      <div class="colophon-avatar">
+        <div class="colophon-avatar-inner">
+          <img
+            class="colophon-avatar-img"
+            :class="faceClicked ? 'is-clicked' : ''"
+            :src="img"
+            :srcset="`${img} 1x, ${img2x} 2x`"
+            alt="😬"
+            draggable="false"
+            aria-hidden
+            title=" ...is there something on my face? 😬"
+            @click="onFaceClick"
+            :style="
+              faceClicked
+                ? `
+							transform: scale(${1 - faceClicked * 0.05});
+							opacity: ${1 - faceClicked * 0.03};
+						`
+                : null
+            "
+          />
+        </div>
+      </div>
 
-			<!-- my face -->
-			<div class="colophon-avatar">
-				<div class="colophon-avatar-inner">
-					<img
-						class="colophon-avatar-img"
-						:class="faceClicked ? 'is-clicked' : ''"
-						:src="img"
-						:srcset="`${img} 1x, ${img2x} 2x`"
-						alt="😬"
-						draggable="false"
-						aria-hidden
-						title=" ...is there something on my face? 😬"
-						@click="onFaceClick"
-						:style="faceClicked ? `
-							transform: scale(${ 1 - (faceClicked * 0.05) });
-							opacity: ${ 1 - (faceClicked * 0.03) };
-						` : null"
-					/>
-				</div>
-			</div>
+      <!-- name & bio -->
+      <div class="colophon-caption">
+        <!-- name -->
+        <span class="colophon-caption-name subhead">
+          {{ headline }}
+        </span>
 
-			<!-- name & bio -->
-			<div class="colophon-caption">
-				<!-- name -->
-				<span class="colophon-caption-name subhead">
-					{{ headline }}
-				</span>
-
-				<!-- bio -->
-				<div
-					class="colophon-caption-bio"
-					:class="headline === site.title && 'u-font-sm'"
-					v-html="$static.d.content"
-				/>
-			</div>
-		</div>
-	</div>
+        <!-- bio -->
+        <div
+          class="colophon-caption-bio"
+          :class="headline === site.title && 'u-font-sm'"
+          v-html="$static.d.content"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-
 <style lang="scss">
+.colophon {
+  border-radius: $radius-md;
+}
 
-	.colophon {
-		border-radius: $radius-md;
-	}
-	
-	// add padding to inner container
-	.colophon-inner {
-		display: flex;
-		padding: $gutter;
+// add padding to inner container
+.colophon-inner {
+  display: flex;
+  padding: $gutter;
 
-		// face on top on small screens
-		@media (max-width: $bp-xxs - 0.001) {
-			flex-wrap: wrap;
-		}
+  // face on top on small screens
+  @media (max-width: $bp-xxs - 0.001) {
+    flex-wrap: wrap;
+  }
 
-		// more padding
-		@media (min-width: $bp-md) {
-			padding: rem(30) rem(40);
-		}
-	}
+  // more padding
+  @media (min-width: $bp-md) {
+    padding: rem(30) rem(40);
+  }
+}
 
-	.colophon-caption-bio > p {
-		line-height: 1.25;
+.colophon-caption-bio > p {
+  line-height: 1.25;
 
-		& + p {
-			margin-top: 0.625em;
-		}
-	}
+  & + p {
+    margin-top: 0.625em;
+  }
+}
 
-	.colophon a {
-		text-decoration-color: $light-2;
+.colophon a {
+  text-decoration-color: $light-2;
 
-		&:hover, &:focus {
-			text-decoration-color: $white;
-		}
-	}
+  &:hover,
+  &:focus {
+    text-decoration-color: $white;
+  }
+}
 
+////////////////////////////////
+// layout
+////////////////////////////////
 
-	////////////////////////////////
-	// layout
-	////////////////////////////////
+// display avatar and caption inline and vertically center
+// avatar sizing
+.colophon-avatar {
+  min-width: calc(#{rem(100)});
 
-	// display avatar and caption inline and vertically center
-	// avatar sizing
-	.colophon-avatar {
-		min-width: calc(#{rem(100)});
+  @media (max-width: $bp-xxs - 0.001) {
+    margin: -0.5em auto 1.5em auto;
+    min-width: calc(#{rem(200)});
+  }
 
-		@media (max-width: $bp-xxs - 0.001) {
-			margin: -0.5em auto 1.5em auto;
-			min-width: calc(#{rem(200)});
-		}
+  @media (min-width: $bp-xxs) {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
 
-		@media (min-width: $bp-xxs) {
-			margin-top: auto;
-			margin-bottom: auto;
-		}
+  // bigger avatar on bigger screens
+  @media (min-width: $bp-lg) {
+    min-width: calc(#{rem(160)});
+  }
+}
 
-		// bigger avatar on bigger screens
-		@media (min-width: $bp-lg) {
-			min-width: calc(#{rem(160)});
-		}
-	}
+.colophon-avatar-inner {
+  // animation: popIn $duration-lg 0.45s; // page transition duration + delay
 
-	.colophon-avatar-inner {
-		// animation: popIn $duration-lg 0.45s; // page transition duration + delay
+  @media (min-width: $bp-xxs) {
+    padding-right: 1em;
+  }
+  @media (min-width: $bp-lg) {
+    padding-right: 2em;
+  }
+}
 
-		@media (min-width: $bp-xxs) {
-			padding-right: 1em;
-		}
-		@media (min-width: $bp-lg) {
-			padding-right: 2em;
-		}
-	}
+.colophon-avatar-img {
+  border-radius: 50%;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  // initial placeholder state
+  font-size: rem(58);
 
-	.colophon-avatar-img {
-		border-radius: 50%;
-		overflow: hidden;
-		width: 100%;
-		height: 100%;
-		// initial placeholder state
-		font-size: rem(58);
+  @media (min-width: $bp-lg) {
+    font-size: rem(80);
+    transform: scale(1.125);
+  }
 
-		@media (min-width: $bp-lg) {
-			font-size: rem(80);
-			transform: scale(1.125);
-		}
+  &:not(.is-clicked):hover {
+    cursor: pointer;
+    animation: pulse 2s 1s;
+  }
+  &.is-clicked {
+    cursor: not-allowed;
+    transition: transform 0.2s cubic-bezier(0, 0.25, 0.6, 4);
+  }
+}
 
-		&:not(.is-clicked):hover {
-			cursor: pointer;
-			animation: pulse 2s 1s;
-		}
-		&.is-clicked {
-			cursor: not-allowed;
-			transition: transform 0.2s cubic-bezier(0, 0.25, 0.6, 4);
-		}
-	}
+// caption sizing
+.colophon-caption {
+}
 
-	// caption sizing
-	.colophon-caption {
-	}
+// JAMES FERRELL positioning
+.colophon-caption-name {
+  display: inline-block; // needed to make span use margin
+  margin-bottom: 0.5em;
+}
 
-	// JAMES FERRELL positioning
-	.colophon-caption-name {
-		display: inline-block; // needed to make span use margin
-		margin-bottom: 0.5em;
-	}
+////////////////////////////////////
+// contextual adjustments
+////////////////////////////////////
 
-
-	////////////////////////////////////
-	// contextual adjustments
-	////////////////////////////////////
-
-	// let it breathe
-	* + .colophon {
-		margin-top: 2rem;
-	}
+// let it breathe
+* + .colophon {
+  margin-top: 2rem;
+}
 </style>
--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -7,154 +7,152 @@
 </static-query>
 
 <script>
-	import site from "../../content/site.json";
+import site from "../../content/site.json";
 
-	export default {
-		name: "HomeHero",
-		props: {
-			headline: { default: "Hello!" },
-			classes:  { default: null },
-			img:      { default: "/images/jamesatar.jpg" },
-			img2x:    { default: "/images/jamesatar@2x.jpg" }
-		},
-		data() {
-			return {
-				site,
-				faceClicked: false
-			}
-		},
-		methods: {
-			onFaceClick: function() {
-				if (!this.faceClicked) alert("Hey, that's not very nice 😬");
-				this.faceClicked++;
-				if (this.faceClicked === 10) alert("Stahp! 😫");
-				if (this.faceClicked >= 20) alert("kbye 👋");
-			}
-		}
-	};
+export default {
+  name: "HomeHero",
+  props: {
+    headline: { default: "Hello!" },
+    classes: { default: null },
+    img: { default: "/images/jamesatar.png" },
+    img2x: { default: "/images/jamesatar@2x.png" },
+  },
+  data() {
+    return {
+      site,
+      faceClicked: false,
+    };
+  },
+  methods: {
+    onFaceClick: function() {
+      if (!this.faceClicked) alert("Hey, that's not very nice 😬");
+      this.faceClicked++;
+      if (this.faceClicked === 10) alert("Stahp! 😫");
+      if (this.faceClicked >= 20) alert("kbye 👋");
+    },
+  },
+};
 </script>
 
-
 <template>
-	<div class="home-hero brand-theme">
-		<!-- my face -->
-		<div class="home-hero-avatar">
-			<img
-				class="home-hero-avatar-img"
-				:class="faceClicked ? 'is-clicked' : ''"
-				:src="img"
-				:srcset="`${img} 1x, ${img2x} 2x`"
-				alt="😬"
-				draggable="false"
-				aria-hidden
-				title=" ...is there something on my face? 😬"
-				@click="onFaceClick"
-				:style="faceClicked ? `
-					transform: scale(${ 1 - (faceClicked * 0.05) });
-					opacity: ${ 1 - (faceClicked * 0.03) };
-				` : null"
-			/>
-		</div>
+  <div class="home-hero brand-theme">
+    <!-- my face -->
+    <div class="home-hero-avatar">
+      <img
+        class="home-hero-avatar-img"
+        :class="faceClicked ? 'is-clicked' : ''"
+        :src="img"
+        :srcset="`${img} 1x, ${img2x} 2x`"
+        alt="😬"
+        draggable="false"
+        aria-hidden
+        title=" ...is there something on my face? 😬"
+        @click="onFaceClick"
+        :style="
+          faceClicked
+            ? `
+					transform: scale(${1 - faceClicked * 0.05});
+					opacity: ${1 - faceClicked * 0.03};
+				`
+            : null
+        "
+      />
+    </div>
 
-		<!-- name & bio -->
-		<h1 class="home-hero-headline">
-			{{ headline }}
-		</h1>
+    <!-- name & bio -->
+    <h1 class="home-hero-headline">
+      {{ headline }}
+    </h1>
 
-		<!-- bio -->
-		<div
-			class="home-hero-bio"
-			v-html="$static.d.content"
-		/>
-	</div>
+    <!-- bio -->
+    <div class="home-hero-bio" v-html="$static.d.content" />
+  </div>
 </template>
 
-
 <style lang="scss">
+.home-hero-avatar {
+  margin-bottom: $gutter; // non-grid fallback
 
-	.home-hero-avatar {
-		margin-bottom: $gutter; // non-grid fallback
+  @media (min-width: $bp-md) {
+    width: rem(160);
+  }
+}
+.home-hero-avatar-img {
+  border-radius: 50%;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  cursor: not-allowed;
+  transition: transform 0.2s cubic-bezier(0, 0.25, 0.6, 4);
+}
 
-		@media (min-width: $bp-md) {
-			width: rem(160);
-		}
-	}
-	.home-hero-avatar-img {
-		border-radius: 50%;
-		overflow: hidden;
-		width: 100%;
-		height: 100%;
-		cursor: not-allowed;
-		transition: transform 0.2s cubic-bezier(0, 0.25, 0.6, 4);
-	}
+.home-hero-bio > p {
+  margin-bottom: 0.625em;
 
-	.home-hero-bio > p {
-		margin-bottom: 0.625em;
+  @media (min-width: $bp-md) {
+    font-size: $font-xl;
+    line-height: 1.333;
+    margin-bottom: 0.666em;
+  }
+}
 
-		@media (min-width: $bp-md) {
-			font-size: $font-xl;
-			line-height: 1.333;
-			margin-bottom: 0.666em;
-		}
-	}
+.home-hero a {
+  text-decoration-color: $light-2;
 
-	.home-hero a {
-		text-decoration-color: $light-2;
+  &:hover,
+  &:focus {
+    text-decoration-color: $white;
+  }
 
-		&:hover, &:focus {
-			text-decoration-color: $white;
-		}
+  @media (min-width: $bp-md) {
+    text-decoration-thickness: 2px;
+  }
+}
 
-		@media (min-width: $bp-md) {
-			text-decoration-thickness: 2px;
-		}
-	}
+////////////////////////////////
+// layout
+////////////////////////////////
 
-	////////////////////////////////
-	// layout
-	////////////////////////////////
+.home-hero {
+  @include hero-base;
 
-	.home-hero {
-		@include hero-base;
+  @supports (display: grid) {
+    align-items: normal; // prevent headline & bio from distributing themselves vertically
 
-		@supports (display: grid) {
-			align-items: normal; // prevent headline & bio from distributing themselves vertically 
+    .home-hero-avatar {
+      grid-column: 1 / 7;
+      margin: auto 0 auto auto;
+    }
 
-			.home-hero-avatar {
-				grid-column: 1 / 7;
-				margin: auto 0 auto auto;
-			}
+    .home-hero-headline {
+      grid-column: 7 / 13;
+      margin: auto auto auto ($gutter / 2);
+    }
 
-			.home-hero-headline {
-				grid-column: 7 / 13;
-				margin: auto auto auto ($gutter / 2);
-			}
+    .home-hero-bio {
+      grid-column: 1 / 13;
+    }
 
-			.home-hero-bio {
-				grid-column: 1 / 13;
-			}
+    @media (min-width: $bp-xs) {
+      .home-hero-bio {
+        grid-column: 2 / 12;
+      }
+    }
 
-			@media (min-width: $bp-xs) {
-				.home-hero-bio {
-					grid-column: 2 / 12;
-				}
-			}
+    @media (min-width: $bp-sm) {
+      .home-hero-avatar {
+        grid-column: 1 / 4;
+        grid-row: 1 / span 2;
+      }
+      .home-hero-headline {
+        margin: auto auto 0 0;
+      }
 
-			@media (min-width: $bp-sm) {
-				.home-hero-avatar {
-					grid-column: 1 / 4;
-					grid-row: 1 / span 2;
-				}
-				.home-hero-headline {
-					margin: auto auto 0 0;
-				}
-
-				.home-hero-headline,
-				.home-hero-bio {
-					grid-column: 4 / 12;
-				}
-			}
-		}
-	}
-
+      .home-hero-headline,
+      .home-hero-bio {
+        grid-column: 4 / 12;
+      }
+    }
+  }
+}
 </style>
